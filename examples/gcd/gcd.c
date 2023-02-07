c2e83c327e5952dbaf574ec66438e31e4f6839b9
Charitha Saumya <cgusthin@purdue.edu>
2023-02-07 12:16:26 -0500

    change symbolic vars in erosion example

--- examples/gcd/gcd.c
+++ examples/gcd/gcd.c
@@ -13,7 +13,7 @@ void foo(int n1, int n2)
         if(n1 > n2)
             n1 -= n2;
         else
-            n2 -= n1;
+            n2 += -n1;
     }
     //printf("GCD = %d",n1);
 
--- examples/setup_env.sh
+++ examples/setup_env.sh
@@ -1,3 +1,3 @@
 # set the paths to correct locations
-export KLEE_HOME=../
-export LLVM_HOME=../../llvm-project-rocm/
+export KLEE_BUILD_DIR=/home/min/a/cgusthin/git/klee/build
+export LLVM_BUILD_DIR=/home/min/a/cgusthin/git/llvm-project-rocm/build_cfmse
