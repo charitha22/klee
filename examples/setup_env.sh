7a68ee65e2d3528ba895581cd0a74d190da1788f
Charitha Saumya <cgusthin@purdue.edu>
2023-03-09 13:03:54 -0500

    update bench

--- examples/setup_env.sh
+++ examples/setup_env.sh
@@ -1,3 +1,6 @@
 # set the paths to correct locations
-export KLEE_BUILD_DIR=/home/ksundar/Dev/klee/build
-export LLVM_BUILD_DIR=/home/ksundar/Dev/llvm-project/build_cfmse
+export KLEE_BUILD_DIR=/home/min/a/cgusthin/git/klee/build
+export LLVM_BUILD_DIR=/home/min/a/cgusthin/git/llvm-project-rocm/build_cfmse
+
+# directory of klee, stp, klee-uclibs, minisat (assuming they are in the same directory)
+export TOOLS_DIR=/home/min/a/cgusthin/git
