d3261c2f0bad9a5aca2da15dfe8b4608de2d3ccc
Kirshanthan Sundararajah <ksundar@purdue.edu>
2023-03-10 12:28:04 -0500

    removing some files

--- examples/setup_env.sh
+++ examples/setup_env.sh
@@ -1,6 +1,3 @@
 # set the paths to correct locations
-export KLEE_BUILD_DIR=/home/min/a/cgusthin/git/klee/build
-export LLVM_BUILD_DIR=/home/min/a/cgusthin/git/llvm-project-rocm/build_cfmse
-
-# directory of klee, stp, klee-uclibs, minisat (assuming they are in the same directory)
-export TOOLS_DIR=/home/min/a/cgusthin/git
+export KLEE_BUILD_DIR=/home/ksundar/Dev/klee/build
+export LLVM_BUILD_DIR=/home/ksundar/Dev/llvm-project/build_cfmse
