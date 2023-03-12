ede1adfc4693d4bc4166ba9756f524115b4ed9cf
Kirshanthan Sundararajah <ksundar@purdue.edu>
2023-03-12 17:55:56 -0400

    transitive closure fix

--- examples/transitive_closure/tc.c
+++ examples/transitive_closure/tc.c
@@ -8,12 +8,12 @@ void transitive_closure(bool *graph, int n) {
     for(int v = 0; v < n; v++) graph[v *n + v] = true;
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            bool d = graph[i * n + k] & graph[k * n + j];
-            if (d | graph[i * n + j]) {
-                graph[i * n + j] = d;
+            for (int j = 0; j < n; j++) {
+                bool d = graph[i * n + k] & graph[k * n + j];
+                if (d | graph[i * n + j]) {
+                    graph[i * n + j] = d;
+                }
             }
-        }
         }
     }
 }
@@ -21,7 +21,7 @@ void transitive_closure(bool *graph, int n) {
 int main() {
     int n = 3;
     bool *graph = (bool*)malloc(n*n*sizeof(bool));
-    klee_make_symbolic(graph, n*n*sizeof(int), "graph");
+    klee_make_symbolic(graph, n*n*sizeof(bool), "graph");
     transitive_closure(graph, n);
     return 0;
 }
