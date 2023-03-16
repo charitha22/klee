0074b141e89513e1617e6851814cc4aaf9661ede
Charitha Saumya <cgusthin@purdue.edu>
2023-03-16 15:50:07 -0400

    bitonic sort bench

--- kernels/bitonic_sort/bitonic_sort.c
+++ kernels/bitonic_sort/bitonic_sort.c
@@ -1,77 +1,106 @@
+/*
+ *  C Program to Implement Bitonic sort
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <klee/klee.h>
+// #define SIZE 8
+#define SWAP(x,y) t = x; x = y; y = t;
+ 
+void compare();
+void bitonicmerge(int, int, int);
+void recbitonic(int, int, int);
+void sort();
+ 
+int data[SIZE];
+int up = 1;
+int down = 0;
+ 
+int main()
+{
+    // int i;
+ 
+    // printf("\nEnter the data");
+    // for (i = 0;i < SIZE ;i++)
+    // {
+    //     scanf("%d", &data[i]);
+    // }
 
-void merge_up(int *arr, int n) {
-  int step = n / 2, i, j, k, temp;
-  while (step > 0) {
-    for (i = 0; i < n; i += step * 2) {
-      for (j = i, k = 0; k < step; j++, k++) {
-        #ifdef MERGE
-        klee_open_merge();
-        #endif
-        if (arr[j] > arr[j + step]) {
-          // swap
-          temp = arr[j];
-          arr[j] = arr[j + step];
-          arr[j + step] = temp;
-        }
-        #ifdef MERGE
-        klee_close_merge();
-        #endif
-      }
+    klee_make_symbolic(data, sizeof(data), "data");
+
+    sort();
+    for (int i = 0;i < SIZE-1;i++)
+    {
+        klee_assert(data[i] <= data[i+1]);
     }
-    step /= 2;
-  }
+    return 0;
 }
-
-void merge_down(int *arr, int n) {
-  int step = n / 2, i, j, k, temp;
-  while (step > 0) {
-    for (i = 0; i < n; i += step * 2) {
-      for (j = i, k = 0; k < step; j++, k++) {
-        #ifdef MERGE
-        klee_open_merge();
-        #endif
-        if (arr[j] < arr[j + step]) {
-          // swap
-          temp = arr[j];
-          arr[j] = arr[j + step];
-          arr[j + step] = temp;
-        }
-        #ifdef MERGE
-        klee_close_merge();
-        #endif
-      }
+/*
+ * compare and swap based on dir
+ */
+void compare(int i, int j, int dir)
+{
+    int t;
+#ifdef MERGE
+    klee_open_merge();
+#endif
+    if (dir == (data[i] > data[j]))
+    {
+        SWAP(data[i], data[j]);
     }
-    step /= 2;
-  }
+#ifdef MERGE
+    klee_close_merge();
+#endif
 }
-
-void printArray(int *arr, int n) {
-  int i;
-
-  printf("[%d", arr[0]);
-  for (i = 1; i < n; i++) {
-    printf(",%d", arr[i]);
-  }
-  printf("]\n");
-}
-
-int main(int argc, char **argv) {
-  int n, *arr, i, s;
-  n = SIZE;
-  // allocate space and read all the numbers
-  arr = (int *)malloc(n * sizeof(int));
-  klee_make_symbolic((int *)arr, n * sizeof(int), "arr");
-  
-  // do merges
-  for (s = 2; s <= n; s *= 2) {
-    for (i = 0; i < n;) {
-      merge_up((arr + i), s);
-      merge_down((arr + i + s), s);
-      i += s * 2;
+/*
+ * Sorts a bitonic sequence in ascending order if dir=1
+ * otherwise in descending order
+ */
+void bitonicmerge(int low, int c, int dir)
+{
+    int k, i;
+#ifdef MERGE
+    klee_open_merge();
+#endif
+    if (c > 1)
+    {
+         k = c / 2;
+        for (i = low;i < low+k ;i++)
+            compare(i, i+k, dir);    
+        bitonicmerge(low, k, dir);
+        bitonicmerge(low+k, k, dir);    
     }
-  }
-
+#ifdef MERGE
+    klee_close_merge();
+#endif
+}
+/*
+ * Generates bitonic sequence by sorting recursively
+ * two halves of the array in opposite sorting orders
+ * bitonicmerge will merge the resultant data
+ */
+void recbitonic(int low, int c, int dir)
+{
+    int k;
+#ifdef MERGE
+    klee_open_merge();
+#endif
+    if (c > 1)
+    {
+        k = c / 2;
+        recbitonic(low, k, up);
+        recbitonic(low + k, k, down);
+        bitonicmerge(low, c, dir);
+    }
+#ifdef MERGE
+    klee_close_merge();
+#endif
+}
+ 
+/*
+ * Sorts the entire array
+ */
+void sort()
+{
+    recbitonic(0, SIZE, up);
 }
