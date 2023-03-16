29938279ab8c0c28bb050270de78d47541196417
Charitha Saumya <cgusthin@purdue.edu>
2023-03-16 16:38:47 -0400

    update makefiles

--- kernels/connected_comp/concomp.c
+++ kernels/connected_comp/concomp.c
@@ -45,5 +45,6 @@ int main() {
     klee_make_symbolic(cc, n*sizeof(int), "cc");
     int numcc = connected_comp(graph, cc, n);
     //printf("number of connected components: %d\n", numcc);
+    klee_assert(numcc <= n && numcc >= 0);
     return 0;
 }
