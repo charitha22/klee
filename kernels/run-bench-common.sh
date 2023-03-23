9d342d861a87530ce742d2646963a6337fbc5a97
Charitha Saumya <cgusthin@purdue.edu>
2023-03-23 14:20:11 -0400

    update to run script

--- kernels/run-bench-common.sh
+++ kernels/run-bench-common.sh
@@ -10,24 +10,23 @@ function run_bench  {
 
     cd $1
     # rm -f *.log *.csv
+    
+    # create a unique output directory for this run
+    OUTPUT_DIR_FOR_THIS_RUN=$BENCH_OUT_HOME/$(date +%m%d%Y_%H%M%S)_"$1"_"$4"
+    mkdir -p $OUTPUT_DIR_FOR_THIS_RUN
+    echo "Output directory: $OUTPUT_DIR_FOR_THIS_RUN"
+
     for input_size in $2; do
         echo "Input size: $input_size"
         make clean > compile_$input_size.log 2>&1
         SIZE=$input_size make all >> compile_$input_size.log 2>&1
+         
         for i in $(seq 1 $3); do
             echo "Run $i"
             echo "running $4"
+                
+            OUTPUT_DIRNAME=$OUTPUT_DIR_FOR_THIS_RUN/"$4"_"$input_size"_"$i"
             
-            # create the output benchmark directory
-            mkdir -p $BENCH_OUT_HOME/$1
-            
-            OUTPUT_DIRNAME=$BENCH_OUT_HOME/$1/"$4"_"$input_size"_"$i"
-            # remove the output directory if it exists
-            if [ -d "$OUTPUT_DIRNAME" ]; then
-                rm -rf "$OUTPUT_DIRNAME"
-            fi
-            
-
             OUTPUT_DIR=$OUTPUT_DIRNAME make "$4" > "$4"_"$input_size"_"$i".log 2>&1
             cat $OUTPUT_DIRNAME/info >> "$4"_"$input_size"_"$i".log 2>&1
             ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv $OUTPUT_DIRNAME > "$4"_stats_"$input_size"_"$i".csv 2>&1
