d6904bc4cddac98cd3e72bbe3cb45094fc2fdabf
Charitha Saumya <cgusthin@purdue.edu>
2023-03-23 14:04:47 -0400

    update to run script

--- kernels/run-bench.sh
+++ kernels/run-bench.sh
@@ -1,4 +1,4 @@
-NRUNS=5
+BENCH_OUT_HOME=/local/scratch/a/$USER/cfmse-experiments
 
 function run_bench  {
     echo "Running $1"
@@ -9,46 +9,30 @@ function run_bench  {
     fi
 
     cd $1
-    rm -f *.log *.csv
+    # rm -f *.log *.csv
     for input_size in $2; do
         echo "Input size: $input_size"
-        make clean >> compile_$input_size.log 2>&1
+        make clean > compile_$input_size.log 2>&1
         SIZE=$input_size make all >> compile_$input_size.log 2>&1
-        for i in $(seq 1 $NRUNS); do
+        for i in $(seq 1 $3); do
             echo "Run $i"
-            # echo "running klee"
-            # make klee >> klee_"$input_size"_"$i".log 2>&1
-            # cat klee-last/info >> klee_"$input_size"_"$i".log 2>&1
-            # ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv klee-last/ >> klee_stats_"$input_size"_"$i".csv 2>&1
-            echo "running klee_sm"
-            make klee_sm >> klee_sm_"$input_size"_"$i".log 2>&1
-            cat klee-last/info >> klee_sm_"$input_size"_"$i".log 2>&1
-            ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv klee-last/ >> klee_sm_stats_"$input_size"_"$i".csv 2>&1
-            echo "running klee_cfm"
-            make klee_cfm >> klee_cfm_"$input_size"_"$i".log 2>&1
-            cat klee-last/info >> klee_cfm_"$input_size"_"$i".log 2>&1
-            ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv klee-last/ >> klee_cfm_stats_"$input_size"_"$i".csv 2>&1
-            echo "running klee_cfmsm"
-            make klee_cfmsm >> klee_cfmsm_"$input_size"_"$i".log 2>&1
-            cat klee-last/info >> klee_cfmsm_"$input_size"_"$i".log 2>&1
-            ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv klee-last/ >> klee_cfmsm_stats_"$input_size"_"$i".csv 2>&1
+            echo "running $4"
+            
+            # create the output benchmark directory
+            mkdir -p $BENCH_OUT_HOME/$1
+            
+            OUTPUT_DIRNAME=$BENCH_OUT_HOME/$1/"$4"_"$input_size"_"$i"
+            # remove the output directory if it exists
+            if [ -d "$OUTPUT_DIRNAME" ]; then
+                rm -rf "$OUTPUT_DIRNAME"
+            fi
+            
+
+            OUTPUT_DIR=$OUTPUT_DIRNAME make "$4" > "$4"_"$input_size"_"$i".log 2>&1
+            cat $OUTPUT_DIRNAME/info >> "$4"_"$input_size"_"$i".log 2>&1
+            ${KLEE_BUILD_DIR}/bin/klee-stats --print-all --table-format=csv $OUTPUT_DIRNAME > "$4"_stats_"$input_size"_"$i".csv 2>&1
         done
     done
     cd ..
 }
 
-
-# run_bench "toupper" "10"
-# run_bench "bitonic_sort" "4"
-# run_bench "connected_comp" "3"
-# run_bench "detect_edges" "3"
-# run_bench "dilation" "4"
-# run_bench "erosion" "4"
-# run_bench "floyd_warshall" "3"
-# run_bench "prim" "3"
-# run_bench "transitive_closure" "3"
-# run_bench "kruskal" "3"
-run_bench "merge_sort" "5"
-
-
-# echo "Done" | mail -s "benchmarks run complete!" $USER
